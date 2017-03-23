c9e5288f470a7e6332f9dd1d8bb7d256dd97e9b3
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-03-23 09:51:16 +0600

    update from home

---
+++ EF.UOWRP/EF.UOWRP.Data/Persistence/PlutoContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF.UOWRP.Data.Core.Domain;
+using EF.UOWRP.Data.Persistence.EntityConfigurations;
+
+namespace EF.UOWRP.Data.Persistence
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
